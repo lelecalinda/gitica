b186304b0a710f70705c56e4451f3da387390006
lelecalinda <142261752+lelecalinda@users.noreply.github.com>
2024-09-25 11:38:05 -0300

    .

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 function soma(num1,num2){
     return num1+num2;
 } 
-console.log (soma(3,4));
+console.log (soma(2,3));
+
+function quadrado(num1) {
+    return num1*num1
+}
+console.log(quadrado(2))
