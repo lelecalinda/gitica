5861eb9a9ccb252645ea402543ace2e6ffbdf988
lelecalinda <142261752+lelecalinda@users.noreply.github.com>
2024-09-29 19:33:46 -0300

    mi

--- main.js
+++ main.js
@@ -27,23 +27,3 @@ let Media = function calculomédia(num1,num2,num3) {
     return (num1+num2+num3)/3
 }
 console.log(Media(2,3,10))
-
-prompt(function soma(num1,num2){
-    return num1+num2;
-} 
-console.log (soma(5,3));
-
-function quadrado(num1) {
-    return num1*num1
-}
-console.log(quadrado(2));
-
-let media = function (n1, n2, n3){ 
-
-     return (n1 + n2 + n3) / 2;
-}
-console.log(media(6, 4, 2));
-
-let numero=prompt("qual a sua idade?")
- document.write(13)
- 
