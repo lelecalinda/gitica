67637683a7fe081b1067c1c59a18be2def12926d
lelecalinda <142261752+lelecalinda@users.noreply.github.com>
2024-09-29 19:31:18 -0300

    ,mk

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
-function soma(num1,num2){
+let CalculodeMediaDoUsuário = function Mediazinha(num1,num2,num3) {
+num1= parselnt(("Me dê o primeiro número"))
+num2 =parseInt(prompt("Me dê o segundo número"))
+num3 =parseInt(prompt("Me dê o terceiro número"))
+return (num1+num2+num3)/3
+}
+console.log(CalculodeMediaDoUsuário())
+
+let Soma = function somaBasica(num1,num2) {
+    return num1+num2
+}
+console.log(Soma(2,3))
+
+let quadrado = function
+areaQuadrado(num1) {
+    return num1*num1
+}
+console.log(Quadrado(2))
+
+function Triangulo(num1,num2,num3) {
+    num3 = 2
+    return (num1*num2)/num3
+}
+console.log(Triangulo(2,3))
+
+let Media = function calculomédia(num1,num2,num3) {
+    return (num1+num2+num3)/3
+}
+console.log(Media(2,3,10))
+
+prompt(function soma(num1,num2){
     return num1+num2;
 } 
 console.log (soma(5,3));
@@ -16,3 +46,4 @@ console.log(media(6, 4, 2));
 
 let numero=prompt("qual a sua idade?")
  document.write(13)
+ 
