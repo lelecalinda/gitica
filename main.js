8c76629b39b7efcff842469c0e02d4273695711b
lelecalinda <142261752+lelecalinda@users.noreply.github.com>
2024-10-02 11:31:06 -0300

    ,N

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
-let CalculodeMediaDoUsuário = function Mediazinha(num1,num2,num3) {
-num1= parselnt(("Me dê o primeiro número"))
-num2 =parseInt(prompt("Me dê o segundo número"))
-num3 =parseInt(prompt("Me dê o terceiro número"))
-return (num1+num2+num3)/3
-}
-console.log(CalculodeMediaDoUsuário())
+// let CalculodeMediaDoUsuário = function Mediazinha(num1,num2,num3) {
+// num1= parselnt(("Me dê o primeiro número"))
+// num2 =parseInt(prompt("Me dê o segundo número"))
+// num3 =parseInt(prompt("Me dê o terceiro número"))
+// return (num1+num2+num3)/3
+// }
+// console.log(CalculodeMediaDoUsuário())
 
-let Soma = function somaBasica(num1,num2) {
-    return num1+num2
-}
-console.log(Soma(2,3))
+// let Soma = function somaBasica(num1,num2) {
+//     return num1+num2
+// }
+// console.log(Soma(2,3))
 
-let quadrado = function
-areaQuadrado(num1) {
-    return num1*num1
-}
-console.log(Quadrado(2))
+// let quadrado = function
+// areaQuadrado(num1) {
+//     return num1*num1
+// }
+// console.log(Quadrado(2))
 
-function Triangulo(num1,num2,num3) {
-    num3 = 2
-    return (num1*num2)/num3
-}
-console.log(Triangulo(2,3))
+// function Triangulo(num1,num2,num3) {
+//     num3 = 2
+//     return (num1*num2)/num3
+// }
+// console.log(Triangulo(2,3))
 
-let Media = function calculomédia(num1,num2,num3) {
-    return (num1+num2+num3)/3
-}
-console.log(Media(2,3,10))
+// let Media = function calculomédia(num1,num2,num3) {
+//     return (num1+num2+num3)/3
+// }
+// console.log(Media(2,3,10))
+
+let estados= ['PR','RS', 'SP', 'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'ES', 'GO', 'MA','MT', 'MS', 'MG', 'PA', 'PB', 'PE','PI', 'RJ','RN','RO', 'RR','SC','SE','TO',DF]
